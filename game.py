540dad0ff2b52d5034b783b28b66af6a7318418e
Max <imax.mr@gmail.com>
2025-02-21 22:27:10 -0700

    adds comments to snowman()

--- game.py
+++ game.py
@@ -17,35 +17,63 @@ def snowman(snowman_word):
 
     # print(snowman_word)
 
+    # creates a constant that stores the length of the snowman word
     WORD_LENGTH = len(snowman_word) 
 
+    # using the snowman word, creates a dictionary that stores the letters and their current guesssed status
     correct_letter_guess_statuses = build_letter_status_dict(snowman_word)
+    # list that stores the guessed incorrect letters
     wrong_guesses_list = []
+    # boolean that stores the puzzle win state
     puzzle_solved = False
 
+    # while loop with the following exit conditions:
+    # - the length of wrong_guesses_list equals or exceeds SNOWMAN_MAX_WRONG_GUESSES
+    # - the puzzle_solved boolean's state is changed to True
     while len(wrong_guesses_list) < SNOWMAN_MAX_WRONG_GUESSES and puzzle_solved is not True:
 
+        # print statement which calls the function that displays the current state of the snowman word
         print(generate_word_progress_string(snowman_word, correct_letter_guess_statuses))
         
+        # calls the function that receives input from the user and, if the input is valid, stores that
+        # value into the user_letter variable
         user_letter = get_letter_from_user(correct_letter_guess_statuses, wrong_guesses_list)
 
+        # if statement with the following conditions that checks if the current input letter is found
+        # in the snowman word's status dictionary
         if user_letter in correct_letter_guess_statuses:
+            # if the letter is found
+
+            # prints a status message and changes the status of the input letter to True, marking it as found
             print("You guessed a letter that's in the word!")
             correct_letter_guess_statuses[user_letter] = True
         else:
+            # if the letter is not found
+
+            #prints a status message and adds the incorrect letter to the wrong_guesses_list
             print(f"The letter '{user_letter}' is not in the word.")
             wrong_guesses_list.append(user_letter)
             print_snowman_graphic(len(wrong_guesses_list))
         
+        # calls the function that checks the game state and assigns the resulting value to puzzle_solved
         puzzle_solved = is_word_guessed(snowman_word, correct_letter_guess_statuses)
     
-    generate_word_progress_string(snowman_word, correct_letter_guess_statuses)
+    # at this point, the game is over
 
+    # prints the final status of the game, whether or not the word was fully guessed.
     print(generate_word_progress_string(snowman_word, correct_letter_guess_statuses))
 
+    # if statement that checks the status of the puzzle_solved boolean variable
     if puzzle_solved:
+        # if puzzle_solved resolves to True
+
+        # print a status message that congratulates the player on win
         print("Congratulations, you win!")
+
     else:
+        # if puzzle_solved resolves to False
+
+        # prints a status message that consolates the player on loss and reveals the snowman_word
         print(f"Sorry, you lose! The word was {snowman_word}")
 
 
